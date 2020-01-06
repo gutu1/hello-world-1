3ac535cb5222458f31d0b5cc96a61285ea0dbf95
gutu1 <55424193+gutu1@users.noreply.github.com>
2020-01-06 23:10:38 +0800

    Add files via upload

---
+++ 最大素数.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void cha(int x, int y){
+	int t;
+	t=x;
+	x=y;
+	y=t;
+}
+void perm(int x, int y){
+	int a[5];
+	if(x+1==y){
+		b[k]
+	}
+} 
+int main(){
+	int a[5], n=0, i=0, t;
+	for(n=0;n<5;n++){
+	scanf("%d",a[n]);
+	}
+	for(n=0;n<5;n++){
+		for(i=n;i<5;i++){
+			if(a[i]>a[n]){
+				t=a[i];
+				a[i]=a[n];
+				a[n]=t;
+			}
+		}
+	}
+	
+}
